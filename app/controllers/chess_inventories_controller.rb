7bc70c74e1d955c1a7c6e702623a48ca895ff8ee
Joshua Schultz <joshuamschultz@gmail.com>
2014-07-09 11:37:51 -0700

    removed views of shelf number

--- app/controllers/chess_inventories_controller.rb
+++ app/controllers/chess_inventories_controller.rb
@@ -64,6 +64,6 @@ class ChessInventoriesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def chess_inventory_params
-      params.require(:chess_inventory).permit(:entrytype, :partnumber, :quantity, :shelfnumber,:location)
+      params.require(:chess_inventory).permit(:entrytype, :partnumber, :quantity,:location)
     end
 end
