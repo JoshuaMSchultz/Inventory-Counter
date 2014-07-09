fec7c648c10bc3acbb0e42c9835b0b0f8534bbbc
Joshua Schultz <joshuamschultz@gmail.com>
2014-07-09 11:25:30 -0700

    Fixed shelf issue

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140707222718) do
+ActiveRecord::Schema.define(version: 20140709182327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20140707222718) do
     t.string   "shelfnumber"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "location"
   end
 
   create_table "will_filter_filters", force: true do |t|
